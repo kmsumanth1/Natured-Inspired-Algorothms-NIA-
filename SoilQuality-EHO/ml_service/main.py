c6d88942e13257caeff15fddca8120134cee2a9d
kmsumanth1 <sumanthkm757@gmail.com>
2026-03-21 15:21:10 +0530

    Fix model loading issue

--- SoilQuality-EHO/ml_service/main.py
+++ SoilQuality-EHO/ml_service/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "../soil_image_model/soil_image_model.keras")
 
-model = tf.keras.models.load_model(model_path)
+model = tf.keras.models.load_model(
+    model_path,
+    compile=False
+)
 
 @app.get("/")
 def home():
